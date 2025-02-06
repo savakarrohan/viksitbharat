d444b6cdf590c89d75939da29bea617382d1db82
Savakar <savakarrohan@gmail.com>
2025-02-06 15:28:17 +0530

    feat: gh-pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   // Output
   // output: "export",
   /* config options here */
+  // BasePath settings for my github page deployment
+  basePath: "/viksitbharath",
   images: {
     domains: ["picsum.photos"],
   },
