b65702ff346d88912d6c2fe3d71e03ac3ed2bf59
Savakar <savakarrohan@gmail.com>
2025-02-14 19:30:18 +0530

    feat: several sections

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,2 +1,1 @@
-self.__RSC_SERVER_MANIFEST =
-  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "DIIp9oZFuHDbWbrS6+fO6/B5QhylZ9MLeC7OFnLERos="\n}';
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DIIp9oZFuHDbWbrS6+fO6/B5QhylZ9MLeC7OFnLERos=\"\n}"
